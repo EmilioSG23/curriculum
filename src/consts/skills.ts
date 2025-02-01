6f0242de410ece50b92a8c8f8fd8d3fb824c77a4
EmilioSG23 <emiliosaenz2001@gmail.com>
2025-01-31 20:45:14 -0500

    Fixes generales

---
+++ src/consts/skills.ts
@@ -0,0 +1,183 @@
+export const SKILLS = [
+    {
+        id: "languages",
+        name: "Lenguajes de Programación",
+        values:[
+            {
+                id: "typescript",
+                name: "TypeScript",
+                color: "#3178C6",
+                text: "white",
+            },
+            {
+                id: "javascript",
+                name: "JavaScript",
+                color: "#f9a200",
+                text: "black"
+            },
+            
+            {
+                id: "css",
+                name: "CSS",
+                color: "#004ce9",
+                text: "white"
+            },
+            {
+                id: "html",
+                name: "HTML",
+                color: "#ff4c1e",
+                text: "white"
+            },
+            {
+                id: "java",
+                name: "Java",
+                color: "white",
+                text: "#c42626"
+            },
+            {
+                id: "python",
+                name: "Python",
+                color: "#0081b4",
+                text: "ffdd44"
+            },
+            {
+                id: "c",
+                name: "C",
+                color: "#3d9bd5",
+                text: "white"
+            },
+            {
+                id: "php",
+                name: "PhP",
+                color: "#4f82b9",
+                text: "black"
+            },
+            {
+                id: "ruby",
+                name: "Ruby",
+                color: "#cc0700",
+                text: "white"
+            },
+            {
+                id: "dart",
+                name: "Dart",
+                color: "#00b8f8",
+                text: "white"
+            },
+            {
+                id: "rust",
+                name: "Rust",
+                color: "gray",
+                text: "white"
+            },
+            {
+                id: "c_sharp",
+                name: "C#",
+                color: "#400095",
+                text: "white"
+            }
+        ]
+    },
+    {
+        id: "frontend",
+        name: "Frameworks Frontend",
+        values: [
+            {
+                id: "angular",
+                name: "Angular",
+                color: "#d32a2d",
+                text: "white"
+            },
+            {
+                id: "ionic",
+                name: "Ionic",
+                color: "#008cff",
+                text: "white"
+            },
+            {
+                id: "flutter",
+                name: "Flutter",
+                color: "#00c6fd",
+                text: "white"
+            },
+            {
+                id: "unity",
+                name: "Unity",
+                color: "white",
+                text: "black"
+            }
+        ]
+    },
+    {
+        id: "backend",
+        name: "Frameworks Backend",
+        values: [
+            {
+                id: "express",
+                name: "Express",
+                color: "#838383",
+                text: "white"
+            },
+            {
+                id: "nodejs",
+                name: "NodeJS",
+                color: "#52b05c",
+                text: "black"
+            },
+            {
+                id: "django",
+                name: "Django",
+                color: "white",
+                text: "black"
+            }
+        ]
+    },
+    {
+        id: "others",
+        name: "Otras herramientas",
+        values: [
+            {
+                id: "sql",
+                name: "SQL",
+                color: "#00bdf4",
+                text: "black"
+            },
+            {
+                id: "mysql",
+                name: "MySQL",
+                color: "#007aa4",
+                text: "#de8638"
+            },
+            {
+                id: "photoshop",
+                name: "Photoshop",
+                color: "#00c9ff",
+                text: "#001721"
+            },
+            {
+                id: "illustrator",
+                name: "Illustrator",
+                color: "#ff7c00",
+                text: "#270b00"
+            },
+            {
+                id: "visual-studio",
+                name: "Visual Studio",
+                color: "#007acf",
+                text: "white"
+            },
+            {
+                id: "netbeans",
+                name: "NetBeans",
+                color: "white",
+                text: "#0091ec"
+            },
+            {
+                id: "javafx",
+                name: "JavaFX",
+                color: "#ff8b00",
+                text: "white"
+            }
+        ]
+    }
+]
