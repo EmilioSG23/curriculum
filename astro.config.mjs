e774a815e0fcdf49f89c1a32505b3ab61b2580e3
EmilioSG23 <emiliosaenz2001@gmail.com>
2025-02-01 20:27:27 -0500

    Configuracion para despliegue con astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "http://emiliosg23.github.io/curriculum",
+  base: "/curriculum/",
   integrations: [tailwind()]
 });
