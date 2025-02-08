3daeced7f6831c62fd52b38c9d527b8c41a08674
EmilioSG23 <emiliosaenz2001@gmail.com>
2025-02-08 18:46:04 -0500

    Algunos fixs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://emiliosg23.github.io/curriculum",
-  base: "/curriculum/",
+  site: 'http://emiliosg23.github.io/curriculum',
+  base: '/curriculum/',
   integrations: [tailwind()]
-});
+})
