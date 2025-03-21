5390500f26a5ae632181a34c2f44cfc0c1c2b2ed
EmilioSG23 <emiliosaenz2001@gmail.com>
2025-03-21 02:33:07 -0500

    Nueva Experiencia Añadida, descripción de proyectos

---
+++ src/consts/experience.ts
@@ -0,0 +1,25 @@
+/* eslint-disable */
+
+export const EXPERIENCE = [
+	{
+		lugar: "Riviera",
+		puesto: "Ayudante Diseño Gráfico",
+		tiempo: "2019",
+		descripcion:
+			"En el año 2019, tuve una experiencia laboral trabajando como pasante dentro de un restaurante, donde era ayudante de la parte del diseño gráfico de la página web, además de hacer trabajos de organización y de ofimática en general. Estuve trabajando alrededor de un mes.",
+	},
+	{
+		lugar: "Espol (MIDI)",
+		puesto: "Desarrollador Móvil",
+		tiempo: "2024",
+		descripcion:
+			"Como parte de mis prácticas de vinculación, participé en el proyecto Midi Espol, donde apoyé en la parte de desarrollo del juego interactivo educativo 'Mi Rutina', en el cual desarrollé nuevos niveles del juego. Además, implementé el mecanismo de comunicación a un dashboard para poder recopilar los resultados obtenidos por los niños.",
+	},
+	{
+		lugar: "Solinal",
+		puesto: "Desarrollador Web Frontend",
+		tiempo: "2025 - actualidad",
+		descripcion:
+			"Como parte de mis prácticas empresariales, fui parte del equipo de Desarrolladores Web de la empresa Solinal",
+	},
+];
