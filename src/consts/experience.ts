6eca908669bd27ea43a0b3bdeadd460489379a05
EmilioSG23 <emiliosaenz2001@gmail.com>
2026-02-06 23:00:15 -0500

    feat: add new full stack developer experience and update existing roles in experience section

--- src/consts/experience.ts
+++ src/consts/experience.ts
@@ -3,23 +3,26 @@
 export const EXPERIENCE = [
 	{
 		lugar: "Riviera",
-		puesto: "Ayudante Diseño Gráfico",
-		tiempo: "2019",
-		descripcion:
-			"En el año 2019, tuve una experiencia laboral trabajando como pasante dentro de un restaurante, donde era ayudante de la parte del diseño gráfico de la página web, además de hacer trabajos de organización y de ofimática en general. Estuve trabajando alrededor de un mes.",
+		puesto: "experience.exp-1.rol",
+		tiempo: "experience.exp-1.time",
+		descripcion: "experience.exp-1.description",
 	},
 	{
 		lugar: "Espol (MIDI)",
-		puesto: "Desarrollador Móvil",
-		tiempo: "2024",
-		descripcion:
-			"Como parte de mis prácticas de vinculación, participé en el proyecto Midi Espol, donde apoyé en la parte de desarrollo del juego interactivo educativo 'Mi Rutina', en el cual desarrollé nuevos niveles del juego. Además, implementé el mecanismo de comunicación a un dashboard para poder recopilar los resultados obtenidos por los niños.",
+		puesto: "experience.exp-2.rol",
+		tiempo: "experience.exp-2.time",
+		descripcion: "experience.exp-2.description",
 	},
 	{
 		lugar: "Solinal",
-		puesto: "Desarrollador Web Frontend",
-		tiempo: "2025 - actualidad",
-		descripcion:
-			"Como parte de mis prácticas empresariales, fui parte del equipo de Desarrolladores Web de la empresa Solinal",
+		puesto: "experience.exp-3.rol",
+		tiempo: "experience.exp-3.time",
+		descripcion: "experience.exp-3.description",
+	},
+	{
+		lugar: "PackVip S.A.S.",
+		puesto: "experience.exp-4.rol",
+		tiempo: "experience.exp-4.time",
+		descripcion: "experience.exp-4.description",
 	},
 ];
